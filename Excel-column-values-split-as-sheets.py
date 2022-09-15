e5e0318a1d5074feb614b9dca3dc49fcd3127f02
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-09-15 20:49:28 +0100

    Update Excel-column-values-split-as-sheets.py

--- Excel-column-values-split-as-sheets.py
+++ Excel-column-values-split-as-sheets.py
@@ -1,25 +1,33 @@
 import pandas as pd
 import os
-os.chdir('M:\Track Splits')
 from pandas import ExcelWriter
+os.chdir('M:\Track Splits')
 
-filename = 'Booked appointments May 2022'
+# path to file to split
+path_to_file = "P:\Analysis_HI\Data Requests\2022\TrakCare Down Time May 2022\Booked appointments May 2022.xlsx"
+
+# define file name for output file
+file_name = os.path.basename(path_to_file).split(.)[0]
+
+# file to split into sheets
 df = pd.read_excel(r"P:\Analysis_HI\Data Requests\2022\TrakCare Down Time May 2022\Booked appointments May 2022.xlsx")
 
+# columns to object
 cols = list(df.columns)
 
+# define column to split use as sheet name
 col_of_interest = 'Doctor'
-col_of_interest_values = list(df[col_of_interest].unique())
 
-# Filter within col_of_interest
-search = ['Dr Ray Armstrong', 'Dr Christopher Holroyd'] 
-df = df[df[col_of_interest].isin(search)]
+# if filtering of col_of_interest required, uncomment the following:
+# search = ['Dr A', 'Dr B'] 
+#df = df[df[col_of_interest].isin(search)]
 
+# define sheet names
 sheet_names = list(df[col_of_interest].unique())
 
-# To Excel object loop through values, filter df, write to sheet and name sheet as per filter value
-w = ExcelWriter(f'{filename}_splits.xlsx')
+# to Excel object loop through values, filter DataFrame, write to sheet and name sheet as per filter value
+w = ExcelWriter(f'{file_name}_splits.xlsx')
 for n in sheet_names:
     res = df[df['Doctor'] == n]
     res.to_excel(w, sheet_name=n, index=False)
-w .save()   
+w .save()
